5014265bca6113621f173554699af995bd4ee6cd
Christian W Binderkrantz <mail@example.com>
2025-01-10 10:13:41 +0100

    add exercise

---
+++ fizz_buzz_sum.py
@@ -0,0 +1,6 @@
+# Write a function fizz_buzz_sum to find the sum of all multiples of 3 or 5 below a target value.
+# For example, if the target value was 10, the multiples of 3 or 5 below 10 are 3, 5, 6, and 9.
+# Because 3+5+6+9=233+5+6+9=23, our function would return 23.
+
+def fizz_buzz_sum(target):
+    return -1
